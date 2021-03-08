ab7dd12c4454252d72cad9fc5ea947abe2df7389
Barret Schloerke <schloerke@gmail.com>
2021-03-08 12:12:57 -0500

    Remove dead code and add `app$waitForValue()` to listen for plot output updates

--- tests/shinytest/mytest.R
+++ tests/shinytest/mytest.R
@@ -6,15 +6,14 @@ app$setInputs(start_date = "2004-01-15")
 app$setInputs(end_date = "2020-02-28")
 app$setInputs(seriousness = "1")
 app$setInputs(submit = "click")
+
+first_plot <- app$waitForValue("plt", iotype = "output", ignore = list(NULL))
 Sys.sleep(2) # sleeping past notifications
 app$snapshotDownload("download_data")
 app$snapshot()
+
 app$setInputs(api = "tylenol")
 app$setInputs(submit = "click")
+app$waitForValue("plt", iotype = "output", ignore = list(first_plot))
 Sys.sleep(2) # sleeping past notifications
 app$snapshot()
-app$setInputs(api = "aspirin")
-app$setInputs(start_date = "2004-01-16")
-app$setInputs(start_date = "2004-01-15")
-app$setInputs(seriousness = "2")
-app$setInputs(seriousness = "1")
--- tests/shinytest/philtest.R
+++ tests/shinytest/philtest.R
@@ -6,9 +6,12 @@ app$setInputs(submit = "click")
 app$setInputs(seriousness = "1")
 app$setInputs(end_date = "2020-03-02")
 app$setInputs(submit = "click")
+first_plot <- app$waitForValue("plt", iotype = "output", ignore = list(NULL))
 Sys.sleep(2) # sleeping past notifications
+
 app$setInputs(api = "a")
 app$setInputs(api = "tylenol")
 app$setInputs(submit = "click")
+first_plot <- app$waitForValue("plt", iotype = "output", ignore = list(first_plot))
 Sys.sleep(2) # sleeping past notifications
 app$snapshot()
